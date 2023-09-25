4ac4e2c8419135a24cefae8496ddd961388c98eb
Tycho584 <tycho.killaars@outlook.com>
2023-09-25 17:58:05 +0200

    WAter

--- hfst_2/opdrachten/opdracht_1.py
+++ hfst_2/opdrachten/opdracht_1.py
@@ -6,6 +6,7 @@ stoppen = True
 while stoppen:
 
     goede_catogorie = True
+    juist_aantal = True
 
     print("Van welke catogorie zou u een grpje willen hebben?:")
     print("1) Maakt mij niet zoveel uit, kies maar.")
@@ -40,29 +41,53 @@ while stoppen:
         goede_catogorie = False
         time.sleep(5)
 
-    if goede_catogorie == True:
-        url = f"https://v2.jokeapi.dev/joke/{url_einde}"
-        response = requests.get(url)
-        response_json = response.json() 
-
-        with open("hfst_2/opdrachten/jokeAPI.json", "w") as file:
-            json.dump(response_json, file)
-            if "joke" in response_json:
-                print(response_json["joke"])
-            else:
-                print(response_json["setup"])
-                print(response_json["delivery"])
-            
+    if goede_catogorie:
+        aantal = int(input("Hoeveel grapjes zou je willen hebben? [1-5]: "))
         print("-"*60)
-        time.sleep(2)
 
-        vraag = input("Wil je nog een mopje horen?: [y/n]: ")
-        if vraag == "y":
-            stoppen = True
-        elif vraag == "n":
+        if aantal < 1 or aantal > 5:
+            juist_aantal = False
+
+        if juist_aantal:
+            if aantal == 1:
+
+                url = f"https://v2.jokeapi.dev/joke/{url_einde}"
+                response = requests.get(url)
+                response_json = response.json()
+
+                with open("hfst_2/opdrachten/jokeAPI.json", "w") as file:
+                    json.dump(response_json, file)
+                    if "joke" in response_json:
+                        print(response_json["joke"])
+                    else:
+                        print(response_json["setup"])
+                        print(response_json["delivery"])
+            else:
+                url = f"https://v2.jokeapi.dev/joke/{url_einde}?amount={aantal}"
+                response = requests.get(url)
+                response_json = response.json()
+
+                for grap in range(aantal):
+
+                    with open("hfst_2/opdrachten/jokeAPI.json", "w") as file:
+                        json.dump(response_json, file)
+                        if "joke" in response_json or "single" in response_json:
+                            print(response_json["jokes"][grap]["joke"])
+                        else:
+                            print(response_json["jokes"][grap]["setup"])
+                            print(response_json["jokes"][grap]["delivery"])
+                            print("="*60)
+                
             print("-"*60)
-            print("-"*60)
-            print("Ok, Het programma wordt stop gezet.")
-            print("-"*60)
-            print("-"*60)
-            stoppen = False
+            time.sleep(2)
+
+            vraag = input("Wil je nog een mopje horen?: [y/n]: ")
+            if vraag == "y":
+                stoppen = True
+            elif vraag == "n":
+                print("-"*60)
+                print("-"*60)
+                print("Ok, Het programma wordt stop gezet.")
+                print("-"*60)
+                print("-"*60)
+                stoppen = False
