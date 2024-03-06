a0b204c67caf684b82c51066946022bab3292cb6
tycho584 <tycho.killaars@outlook.com>
2024-03-06 09:29:06 +0100

    1e oefenmee's gemaakt

--- hfst_6/oefenmee/oefenmee_4.py
+++ hfst_6/oefenmee/oefenmee_4.py
@@ -1,6 +1,19 @@
 # Bepaal de faculteit van een getal met behulp van een while-loop.
 
+def facul(getal:int) -> None:
 
+    faculteit = 1
+    uitkomst = 0
+    while True:
+        if getal == 1:
+            break
+
+        getal = getal * faculteit
+        
+
+        
+
+        
 
 print( facul(1) ) # 1
 print( facul(2) ) # 2
